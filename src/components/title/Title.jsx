bc810f838e0e387b347db5ab3edfd24e0d644d9a
EliasSilva1702 <eliassilva170206@gmail.com>
2024-10-29 22:48:01 -0300

    "fix title"

--- src/components/title/Title.jsx
+++ src/components/title/Title.jsx
@@ -3,7 +3,7 @@ import '../../App.css'
 export const Title = () => {
   return (
     <h1 className="text-7xl px-3 font-bold text-red-500 mb-4 cookie-font">
-    Rueda de la Fortuna
+    Galleta de la Fortuna
   </h1>
   )
 }
